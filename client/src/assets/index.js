18ba217296fe7ebe93445c52327757b7eddb7c5a
Siavash Ahmadi <sahmadi4@student.gsu.edu>
2023-03-28 17:01:50 -0400

    changed postRoutes to have ending / to prevent Safari/iOS issues

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import siavashLogo from './siavashLogo.svg';
 
 export {
   download,
   logo,
   preview,
+  siavashLogo,
 };
